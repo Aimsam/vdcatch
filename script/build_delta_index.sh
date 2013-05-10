ffbc337edb29022c7fb1d0df4784bb01876dabfc
aimsam <aimsam@macbook.local>
2013-05-10 21:22:56 +0800

    a

--- script/build_delta_index.sh
+++ script/build_delta_index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #建立增量索引
-/usr/local/coreseek/bin/indexer -c /usr/local/coreseek/etc/sphinx-vd.conf delta --rotate
+/usr/local/coreseek/bin/indexer -c /usr/local/coreseek/etc/sphinx-vd.conf delta --rotate >> /usr/local/coreseek/log/delta.log
 #合并索引
-/usr/local/coreseek/bin/indexer --merge video delta --merge-dst-range deleted 0 0 --rotate -c /usr/local/coreseek/etc/sphinx-vd.conf
+/usr/local/coreseek/bin/indexer --merge video delta --merge-dst-range deleted 0 0 --rotate -c /usr/local/coreseek/etc/sphinx-vd.conf >> /usr/local/coreseek/log/delta.log
--- script/build_main_index.sh
+++ script/build_main_index.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #停止searchd
-/usr/local/coreseek/bin/searchd -c /usr/local/coreseek/etc/sphinx-vd.conf --stop
+/usr/local/coreseek/bin/searchd -c /usr/local/coreseek/etc/sphinx-vd.conf --stop >> /usr/local/coreseek/log/main.log
 #建立主索引
-/usr/local/coreseek/bin/indexer -c /usr/local/coreseek/etc/sphinx-vd.conf video
+/usr/local/coreseek/bin/indexer -c /usr/local/coreseek/etc/sphinx-vd.conf video >> /usr/local/coreseek/log/main.log
 #启动daemon
-/usr/local/coreseek/bin/searchd -c /usr/local/coreseek/etc/sphinx-vd.conf
+/usr/local/coreseek/bin/searchd -c /usr/local/coreseek/etc/sphinx-vd.conf >> /usr/local/coreseek/log/main.log
 
